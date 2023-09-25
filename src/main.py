330f1e84e0ae135fe26d6a878ef1a3195850b1f4
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-24 20:37:13 -0400

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def my_function(x):
+  
+  """Returns the square of the input number.
+  Args:
+    x: A number.
+  Returns:
+    The square of the input number.
+  """
+  return x**2
+
+if __name__ == '__main__':
+  
+    x = 2
+    print(my_function(x))
