e714c311590b9fc13094f6bc557a3a9527b576d5
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-24 20:52:13 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,14 +1,9 @@
 def my_function(x):
-  
-  """Returns the square of the input number.
-  Args:
-    x: A number.
-  Returns:
-    The square of the input number.
-  """
+"""Returns the square of the input number.
+Args: x: A number.
+Returns:The square of the input number.
+"""
   return x**2
-
 if __name__ == '__main__':
-  
-    x = 2
-    print(my_function(x))
+  x = 2
+  print(my_function(x))
