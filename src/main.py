14110e9964dd155a86132d8cb6fd7ac037d03d42
shailajgg <142270941+shailajgg@users.noreply.github.com>
2023-09-24 20:59:07 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,8 +3,9 @@
   Returns:The square of the input number.
 """
 def my_function(x):
-  return x**2
+    return x**2
   
 if __name__ == '__main__':
-  x = 2
-  print(my_function(x))
+    x = 2
+  
+    print(my_function(x))
